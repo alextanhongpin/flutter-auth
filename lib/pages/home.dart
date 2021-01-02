22484fd9a0f6262aeef4fd2ddc5dc9083ec9b360
alextanhongpin <alextan220990@gmail.com>
2021-01-02 12:19:32 +0800

    chore: style home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,29 +5,51 @@ import 'package:provider/provider.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String user = context.watch<AuthModel>().user.email;
+
     return Scaffold(
         appBar: AppBar(
           title: Text('Home'),
         ),
-        body: Center(
-            child: Column(
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text(
+                "Welcome ${user}",
+                style: TextStyle(fontSize: 20.0),
+              ),
+
+              Row(
                 children: [
-                  Text(context.watch<AuthModel>().user.email),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/about');
-                      },
-                      child: Text('Go to about')
+                  Image(
+                    width: 160.0,
+                    image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      context.read<AuthModel>().logout();
-                    },
-                    child: Text('Logout')
+                  Column(
+                    children: [
+                      Text('Title', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
+                      Text('Description')
+                    ],
                   )
-                ]
-            )
+                ],
+              ),
+
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/about');
+                  },
+                  child: Text('Go to about')
+              ),
+              TextButton(
+                  onPressed: () {
+                    context.read<AuthModel>().logout();
+                  },
+                  child: Text('Logout')
+              )
+            ]
         )
+      ),
     );
   }
 }
